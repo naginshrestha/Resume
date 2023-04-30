45dbfeec8f5d26f65770a9cb99c5fbd396b6b29a
mahatsumit5 <mahatsumit5@gmail.com>
2023-04-30 17:23:29 +1000

    new update

--- src/components/my-resume/MyResume.js
+++ src/components/my-resume/MyResume.js
@@ -1,121 +1,151 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react';
-import { Button, Container, Form } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux';
-import {addResumeAction} from '../../actions/actionResume'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { addResumeAction } from "../../actions/actionResume";
 
 export const MyResume = () => {
-    const { user } = useSelector((state) => state.user);
+  const { user } = useSelector((state) => state.user);
 
-    const [skill, setSkill] = useState('');
-    const [skills, setSkills] = useState([]);
-    const [city, setCity] = useState('');
-    const [sunmmary, setSummary] = useState('');
-    const [data, setData] = useState({});
-    const dispatch = useDispatch();
+  const [skill, setSkill] = useState("");
+  const [skills, setSkills] = useState([]);
+  const [city, setCity] = useState("");
+  const [sunmmary, setSummary] = useState("");
+  const [data, setData] = useState({});
+  const dispatch = useDispatch();
 
+  const [resumeData, setResumeData] = useState([]);
 
+  useEffect(() => {}, [user]);
 
-    const [resumeData,setResumeData] =useState([])
+  const handleChanged = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+  };
 
-    useEffect(() => {
+  const handleSumit = (e) => {
+    e.preventDefault();
+    const obj = { ...data, uid: user.uid };
 
-      }, [user]);
-
-    const handleChanged =(e) =>{
-        const {name , value} = e.target;
-        setData({...data,[name]: value,});
-
-
-    }
-
-
-    const handleSumit =(e) =>{
-        e.preventDefault();
-        const obj = { ...data, uid: user.uid };
-
-        console.log("oooo",obj);
-        dispatch(addResumeAction(obj));
-
-
-    }
-
+    console.log("oooo", obj);
+    dispatch(addResumeAction(obj));
+  };
 
   return (
     <>
-        <Form onSubmit={handleSumit} className=' mt-5 w-75 p-3 shadow  justify-content-between align-items-center'>   
-    
-    <h2 className='text-center'>Add Resume</h2>
+      <div class="resumewrapper">
+        <Form
+          onSubmit={handleSumit}
+          className=" mt-5  p-3 shadow  justify-content-between align-items-center"
+        >
+          <h2 className="text-center">Add Resume</h2>
 
-    <hr/>
+          <hr />
 
-   
+          <div className="addresume d-flex flex-row justify-content-around align-items-center">
+            <Form.Group className="mt-2 ">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                type="text"
+                name="fname"
+                onChange={handleChanged}
+                placeholder="Enter F-Name"
+              />
+            </Form.Group>
 
-    <div className='d-flex flex-row justify-content-around align-items-center'>
+            <Form.Group className="mt-2 ">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                type="text"
+                name="lname"
+                onChange={handleChanged}
+                placeholder="Enter L-Name"
+              />
+            </Form.Group>
 
-    <Form.Group className='mt-2 '>
-    <Form.Label>First Name</Form.Label>
-    <Form.Control type='text'  name='fname' onChange={handleChanged} placeholder='Enter F-Name'  />
-    </Form.Group>
+            <Form.Group className="mt-2 ">
+              <Form.Label>Contact</Form.Label>
+              <Form.Control
+                type="number"
+                name="contact"
+                onChange={handleChanged}
+                placeholder="Enter Contact"
+              />
+            </Form.Group>
+          </div>
+          <div className="d-flex flex-row justify-content-around align-items-center">
+            <Form.Group className="mt-2 ">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type="text"
+                name="email"
+                onChange={handleChanged}
+                placeholder="Enter Email Address"
+              />
+            </Form.Group>
 
-    <Form.Group className='mt-2 '>
-    <Form.Label>Last Name</Form.Label>
-    <Form.Control type='text'   name='lname' onChange={handleChanged} placeholder='Enter L-Name' />
-    </Form.Group>
+            <Form.Group className="mt-2 ">
+              <Form.Label>Country</Form.Label>
+              <Form.Control
+                type="text"
+                name="county"
+                onChange={handleChanged}
+                placeholder="Enter Country"
+              />
+            </Form.Group>
 
-    <Form.Group className='mt-2 '>
-    <Form.Label>Contact</Form.Label>
-    <Form.Control type='number'  name='contact' onChange={handleChanged} placeholder='Enter Contact' />
-    </Form.Group>
+            <Form.Group className="mt-2 ">
+              <Form.Label>City</Form.Label>
+              <Form.Control
+                type="text"
+                name="city"
+                onChange={handleChanged}
+                placeholder="Enter City"
+              />
+            </Form.Group>
+          </div>
 
-    </div>
-    <div className='d-flex flex-row justify-content-around align-items-center'>
+          <hr />
+          <h4>Professional Summary:</h4>
+          <div>
+            <Form.Control
+              name="summary"
+              onChange={handleChanged}
+              as="textarea"
+              rows={5}
+            />
+          </div>
 
-<Form.Group className='mt-2 '>
-<Form.Label>Email Address</Form.Label>
-<Form.Control type='text'   name='email' onChange={handleChanged} placeholder='Enter Email Address' />
-</Form.Group>
+          <hr />
+          <h4>Employment History:</h4>
+          <div>
+            <Form.Group className="mt-2  d-flex justify-content-center align-items-center">
+              {/* <Form.Label>Add+ </Form.Label> */}
+              <Form.Control
+                type="text"
+                name="history"
+                onChange={handleChanged}
+                placeholder="Enter History"
+              />
+            </Form.Group>
+          </div>
 
-<Form.Group className='mt-2 '>
-<Form.Label>Country</Form.Label>
-<Form.Control type='text' name='county' onChange={handleChanged} placeholder='Enter Country' />
-</Form.Group>
+          <hr />
+          <h4>Education :</h4>
+          <div>
+            <Form.Group className="mt-2  d-flex justify-content-center align-items-center">
+              {/* <Form.Label>Add+ </Form.Label> */}
+              <Form.Control
+                type="text"
+                name="education"
+                onChange={handleChanged}
+                placeholder="Enter Education"
+              />
+            </Form.Group>
+          </div>
 
-<Form.Group className='mt-2 '>
-<Form.Label>City</Form.Label>
-<Form.Control type='text' name='city' onChange={handleChanged} placeholder='Enter City' />
-</Form.Group>
-
-     </div>
-
-     <hr/>
-     <h4>Professional Summary:</h4>
-     <div>
-
-        <Form.Control name='summary' onChange={handleChanged} as="textarea" rows={5}/>
-
-     </div>
-
-     <hr/>
-     <h4>Employment History:</h4>
-     <div>
-     <Form.Group className='mt-2  d-flex justify-content-center align-items-center'>
-    {/* <Form.Label>Add+ </Form.Label> */}
-    <Form.Control type='text' name='history' onChange={handleChanged} placeholder='Enter History' />
-    </Form.Group>
-     </div>
-
-     <hr/>
-     <h4>Education :</h4>
-     <div>
-     <Form.Group className='mt-2  d-flex justify-content-center align-items-center'>
-    {/* <Form.Label>Add+ </Form.Label> */}
-    <Form.Control type='text' name='education' onChange={handleChanged} placeholder='Enter Education' />
-    </Form.Group>
-     </div>
-
-     <hr/>
-     {/* <h4>Skills :</h4>
+          <hr />
+          {/* <h4>Skills :</h4>
      <div className='d-flex justify-content-center gap-2 align-items-center'>
      <Form.Control
             type="text"
@@ -128,16 +158,18 @@ export const MyResume = () => {
     
      </div> */}
 
-     <div className='d-grid m-auto'>
-        <Button variant='dark mt-4 rounded-pill' type='submit'>Sumit</Button>
-
-     </div>
-
-
-
-     </Form>
+          <div class="buttonwrapper">
+            <Button class="button" type="reset">
+              Reset
+            </Button>
+          </div>
+          <div class="buttonwrapper">
+            <Button class="button" type="submit">
+              Submit
+            </Button>
+          </div>
+        </Form>
+      </div>
     </>
-
-
-  )
-}
+  );
+};
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import { AdminLayout } from "../../components/layout/AdminLayout";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import Table from "react-bootstrap/Table";
+//import Table from "react-bootstrap/Table";
 import ListGroup from "react-bootstrap/ListGroup";
-import {applyjobAction} from '../../actions/applyjobAction'
+import { applyjobAction } from "../../actions/applyjobAction";
 
 import Card from "react-bootstrap/Card";
 // import ListGroup from "react-bootstrap/ListGroup";
@@ -13,124 +13,117 @@ import { useDispatch, useSelector } from "react-redux";
 export const Dashboard = () => {
   const dispatch = useDispatch();
 
-
   const { resume } = useSelector((state) => state.resume);
   const { jobs } = useSelector((state) => state.applyjob);
 
-
   const [data, setData] = useState({});
 
-  const handleChanged =(e) =>{
-    const {name , value} = e.target;
-    setData({...data,[name]: value,});
+  const handleChanged = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+  };
 
-  }
-
-
-  const handleSumit =(e) =>{
+  const handleSumit = (e) => {
     e.preventDefault();
     dispatch(applyjobAction(data));
+  };
 
-
-}
-
- const handleClick =(e) =>{
-
-  console.log("ui");
-
- }
-
+  const handleClick = (e) => {
+    console.log("ui");
+  };
 
   return (
-
     <AdminLayout>
       <div>
         <div className="m-5 p b-solid c-black shadow p-5">
-         
-
           <Form onSubmit={handleSumit} className="mt-5">
-          <h1 className="text-center mt-2">Jobs you want to apply</h1>
-
+            <h1 className="text-center mt-2">Jobs you want to apply</h1>
             <Form.Label htmlFor="basic-url">Your Job link</Form.Label>
-  
-              <Form.Control id="basic-url" type="text" name="url"  onChange={handleChanged} />
-        
+            <Form.Control
+              id="basic-url"
+              type="text"
+              name="url"
+              onChange={handleChanged}
+            />
             <Form.Label>Job Title</Form.Label>
-            <Form.Control required type="text" name="title"  onChange={handleChanged} placeholder="Job Title" />
-
-
+            <Form.Control
+              required
+              type="text"
+              name="title"
+              onChange={handleChanged}
+              placeholder="Job Title"
+            />
             <Form.Group
               className="mb-3"
               controlId="exampleForm.ControlTextarea1"
             >
               <Form.Label>Job Description Requirements</Form.Label>
-              <Form.Control as="textarea" name="description"  onChange={handleChanged} rows={3} />
+              <Form.Control
+                as="textarea"
+                name="description"
+                onChange={handleChanged}
+                rows={3}
+              />
             </Form.Group>
             <Form.Group controlId="date" className="mb-3">
               <Form.Label>Applied Date</Form.Label>
-              <Form.Control type="date" name="applydata"  onChange={handleChanged} />
+              <Form.Control
+                type="date"
+                name="applydata"
+                onChange={handleChanged}
+              />
             </Form.Group>
             <Form.Group controlId="date" className="mb-3">
-              <Form.Label>Due Date</Form.Label>
-              <Form.Control type="date"  name="duedate"  onChange={handleChanged}/>
+              <Form.Label>Deadline</Form.Label>
+              <Form.Control
+                type="date"
+                name="duedate"
+                onChange={handleChanged}
+              />
             </Form.Group>
-            <Button size="lg" type="submit">Submit</Button>{" "}
+            <Button class="button" type="submit">
+              Submit
+            </Button>
+            <Button class="button" type="reset">
+              Reset
+            </Button>
           </Form>
         </div>
 
-        <div className="m-5    g-3 justify-content-around d-grid  p-3 align-items-center  shadow ">
-          <h1 className="text-center mt-5">List of Applied jobs</h1>
+        <div className="wrapper ">
+          <h1 className="text-center mt-5">List of Saved jobs</h1>
+          <div>
+            <div className="  card-list  p-2 shadow">
+              <h5 className="text-center">Apply</h5>
 
-          <div className="d-flex gap-4 flex-column justify-content-between align-items-center">
+              {jobs.map((item) => {
+                console.log("tt", item);
+                return (
+                  <div
+                    onClick={handleClick}
+                    className="p-3 square border border-dark mb-2"
+                  >
+                    <p className="text-center ">{item.title}</p>
 
-          <div className="  card-list  p-2 shadow">
-            <h5 className="text-center">Apply</h5>
-
-            {
-              jobs.map((item)=>{
-                console.log("tt",item);
-                return(
-                  <div onClick={handleClick} className="p-3 square border border-dark mb-2"  >
-                  <p className="text-center ">{item.title}</p>
+                    <a href="{item.url}" target="blank">
+                      Apply now
+                    </a>
                   </div>
-
-
-                    
-                 
-                )
-              })
-
-            }
-
-
+                );
+              })}
+            </div>
+            <div className="  card-list  p-2 shadow">
+              <h5 className="text-center">Pending</h5>
+            </div>
+            <div className="  card-list  p-2 shadow">
+              <h5 className="text-center">Accepted</h5>
+            </div>
+            <div className="  card-list  p-2 shadow">
+              <h5 className="text-center">Rejected</h5>
+            </div>
           </div>
-
-          <div className="  card-list  p-2 shadow">
-            <h5 className="text-center">Pending</h5>
-
-
-          </div>
-
-          <div className="  card-list  p-2 shadow">
-            <h5 className="text-center">Accepted</h5>
-
-
-          </div>
-
-          <div className="  card-list  p-2 shadow">
-            <h5 className="text-center">Rejected</h5>
-
-
-          </div>
-            
-          </div>
-
-
-
- 
         </div>
       </div>
-
     </AdminLayout>
   );
 };
